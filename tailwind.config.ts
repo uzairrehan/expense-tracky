1615bb39fd4de103ec31eba9b474b81a8e0e2779
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-10-01 06:23:42 +0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        creamWhite: '#f3f6e7',
-        darkGreen: '#005942',
-        lightGreen: '#8fc43d',
-        persianGreen: '#00aaab',
-        myBlack:'#333132',
-        myLightGray:'#f2f2f2',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
